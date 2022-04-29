35a13c3dd7025aa63139c667f7bbbc3272e6f504
Agustin Nieto <agustin.nieto@perficient.com>
2022-04-28 21:14:45 -0500

    Adding actual functionality

---
+++ src/components/Order/Order.js
@@ -0,0 +1,34 @@
+import { useGet } from 'restful-react';
+import { Card, CardContent, CircularProgress } from '@mui/material';
+
+const Order = (props) => {
+    const { id } = props;
+
+    const {
+        data: orderData,
+        loading: orderLoading,
+        error: orderError,
+        response: orderResponse,
+    } = useGet({ path: 'order/show/' + id });
+
+    if (orderLoading) {
+        return <CircularProgress />;
+    }
+
+    if (orderError || !orderData.ok) {
+        return <div>Something happened 😔</div>;
+    }
+
+    const order = orderData.order;
+
+    return (
+        <Card>
+            <h1>Order #{id}</h1>
+            <CardContent>
+                <pre>{JSON.stringify(order, null, 2)}</pre>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default Order;
--- src/components/OrderShowPage/OrderShowPage.js
+++ src/components/OrderShowPage/OrderShowPage.js
@@ -1,5 +1,10 @@
+import { useParams } from 'react-router-dom';
+
+import Order from '../Order';
+
 const OrderShowPage = (props) => {
-    return <div>Order Show Page</div>;
+    const params = useParams();
+    return <Order id={params.id} />;
 };
 
 export default OrderShowPage;
