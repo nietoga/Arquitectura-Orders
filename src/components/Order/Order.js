9558eac8f30ea6a971ec9a59a3989bdf7697be08
Agustin Nieto <agustin.nieto@perficient.com>
2022-04-30 08:08:28 -0500

    Improve order show page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,14 +1,64 @@
+import { Button, CircularProgress, TextField } from '@mui/material';
+import { useFormik } from 'formik';
+import { useCallback, useEffect } from 'react';
 import { useGet } from 'restful-react';
-import { Card, CardContent, CircularProgress } from '@mui/material';
+import * as yup from 'yup';
+
+const validationSchema = yup.object({
+    status: yup.string('Enter order status').required('Order status is required'),
+    totalPrice: yup.string("Enter order's total price").required("Order's total price is required"),
+    address: yup
+        .string("Enter order's delivery address")
+        .required("Order's delivery address is required"),
+    customerName: yup.string('Enter customer name').required('Customer name is required'),
+    customerEmail: yup
+        .string('Enter customer email')
+        .email('Enter a valid email')
+        .required('Customer email is required'),
+});
 
 const Order = (props) => {
     const { id } = props;
+
+    const handleSubmit = useCallback(
+        (values) => {
+            if (id) {
+                alert('editing');
+            } else {
+                alert('new');
+            }
+
+            alert(JSON.stringify(values, null, 2));
+        },
+        [id]
+    );
+
+    const formik = useFormik({
+        initialValues: {
+            status: '',
+            totalPrice: '',
+            address: '',
+            product: '',
+            customerName: '',
+            customerEmail: '',
+        },
+        validationSchema: validationSchema,
+        onSubmit: handleSubmit,
+    });
 
     const {
         data: orderData,
         loading: orderLoading,
         error: orderError,
     } = useGet({ path: `order/show/${id}` });
+
+    const order = orderData?.order;
+
+    useEffect(() => {
+        if (order) {
+            formik.setValues(order);
+        }
+    }, [order]);
 
     if (orderLoading) {
         return <CircularProgress />;
@@ -18,15 +68,72 @@ const Order = (props) => {
         return <div>Something happened 😔</div>;
     }
 
-    const order = orderData.order;
-
     return (
-        <Card>
-            <h1>Order #{id}</h1>
-            <CardContent>
-                <pre>{JSON.stringify(order, null, 2)}</pre>
-            </CardContent>
-        </Card>
+        <form onSubmit={formik.handleSubmit}>
+            <TextField
+                fullWidth
+                id="totalPrice"
+                name="totalPrice"
+                label="Total Price"
+                value={formik.values.totalPrice}
+                onChange={formik.handleChange}
+                error={formik.touched.totalPrice && Boolean(formik.errors.totalPrice)}
+                helperText={formik.touched.totalPrice && formik.errors.totalPrice}
+            />
+            <TextField
+                fullWidth
+                id="status"
+                name="status"
+                label="Order Status"
+                value={formik.values.status}
+                onChange={formik.handleChange}
+                error={formik.touched.status && Boolean(formik.errors.status)}
+                helperText={formik.touched.status && formik.errors.status}
+            />
+            <TextField
+                fullWidth
+                id="address"
+                name="address"
+                label="Address"
+                value={formik.values.address}
+                onChange={formik.handleChange}
+                error={formik.touched.address && Boolean(formik.errors.address)}
+                helperText={formik.touched.address && formik.errors.address}
+            />
+            <TextField
+                fullWidth
+                id="customerName"
+                name="customerName"
+                label="Customer Name"
+                value={formik.values.customerName}
+                onChange={formik.handleChange}
+                error={formik.touched.customerName && Boolean(formik.errors.customerName)}
+                helperText={formik.touched.customerName && formik.errors.customerName}
+            />
+            <TextField
+                fullWidth
+                id="customerEmail"
+                name="customerEmail"
+                label="Customer Email"
+                value={formik.values.customerEmail}
+                onChange={formik.handleChange}
+                error={formik.touched.customerEmail && Boolean(formik.errors.customerEmail)}
+                helperText={formik.touched.customerEmail && formik.errors.customerEmail}
+            />
+            <TextField
+                fullWidth
+                id="product"
+                name="product"
+                label="Product"
+                value={formik.values.product}
+                onChange={formik.handleChange}
+                error={formik.touched.product && Boolean(formik.errors.product)}
+                helperText={formik.touched.product && formik.errors.product}
+            />
+            <Button fullWidth color="primary" variant="contained" type="submit">
+                Submit
+            </Button>
+        </form>
     );
 };
 
